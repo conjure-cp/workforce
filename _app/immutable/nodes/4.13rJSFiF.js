eee4bf646a45bce063237f1c8cf7d562635ce513
gskorokhod <64529579+gskorokhod@users.noreply.github.com>
2024-07-15 09:02:11 +0000

    Deploying to gh-pages from @ conjure-cp/workforce@b5490b9212877b6287d517b2de9671e5379458ba 🚀

---
+++ _app/immutable/nodes/4.13rJSFiF.js
@@ -0,0 +1,1 @@
+import{s as o}from"../chunks/scheduler.D3XHxxAF.js";import{S as t,i as n}from"../chunks/index.CA_kXnSj.js";import{e as r}from"../chunks/stores.DJZSo5FR.js";function a(s){return r.subscribe(e=>{}),[]}class p extends t{constructor(e){super(),n(this,e,a,null,o,{})}}export{p as component};
